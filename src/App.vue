2d27a97588af0c7364fe1b2e97dd7f41936d2172
Maxime Daraize <leeroydevassistant2@gmail.com>
2020-09-26 10:14:03 -0400

    add todo ,mark todo done, remove individual and all todos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Vue 3 Todo App</h1>
+<form @submit.prevent="addNewTodo">
+    <label for="">new todo</label> <br />
+    <input v-model="newTodo" type="text" name="" id="" />
+    <button>add new todo</button>
+</form>
+
+<ul v-for="(todo, index) in todos" :key="todo.id">
+    <li @click="toggleDone(todo)" :class="{ done: todo.done }">
+        {{ todo.content }}
+        <button @click="removeTodo(index)">remove todo</button>
+    </li>
+</ul>
+<button @click="removeAllTodo">Remove All Todo</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+    ref
+} from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    setup() {
+        const newTodo = ref('');
+        const todos = ref([]);
+
+        function addNewTodo() {
+            todos.value.push({
+                done: false,
+                content: newTodo.value,
+            });
+            newTodo.value = '';
+        }
+
+        function toggleDone(todo) {
+            todo.done = !todo.done;
+        }
+
+        function removeTodo(index) {
+            todos.value.splice(index, 1);
+        }
+
+        function removeAllTodo() {
+            todos.value = [];
+        }
+
+        return {
+            newTodo,
+            todos,
+            addNewTodo,
+            toggleDone,
+            removeTodo,
+            removeAllTodo,
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.done {
+    text-decoration: line-through;
 }
 </style>
